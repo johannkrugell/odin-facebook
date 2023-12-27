222deea3d7350c60e189f20728bbb22fda0e4fe4
johannkrugell <johannkrugell@gmail.com>
2023-12-28 06:29:49 +1100

    Add Gravatar to the application Add Gravatar helper to the application helper. Update the navbar to show the gravatar image. Update the posts index and other views to show the gravatar image.
    On branch gravatar
    Changes to be committed:
    	modified:   app/helpers/application_helper.rb
    	modified:   app/views/friendships/_confirmed.html.erb
    	modified:   app/views/friendships/_requests.html.erb
    	modified:   app/views/friendships/_suggestions.html.erb
    	modified:   app/views/posts/index.html.erb
    	modified:   app/views/shared/_navbar.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@
 
 # This is the ApplicationHelper module.
 module ApplicationHelper
+  # Generates a Gravatar URL for a given user.
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    identicon = 'd=monsterid'
+    "https://www.gravatar.com/avatar/#{hash}?s=#{size}&#{identicon}"
+  end
 end
